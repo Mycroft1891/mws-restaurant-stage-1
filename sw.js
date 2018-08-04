aed4a0e720b76197b78334e68c8ea322eaf3296a
Mycroft1891 <mycroft1891@gmail.com>
2018-08-04 15:42:35 +0200

    fix fetch requests and use claim to take control

--- sw.js
+++ sw.js
@@ -1,80 +1,57 @@
-// declare cache names
 let cacheVersion    = '-v4';
 let staticCacheName = 'restaurant';
-let currentCache    = staticCacheName + cacheVersion;
 let imageCacheName  = 'restaurant-imgs';
+let currentCache    = staticCacheName + cacheVersion;
 let currentImages   = imageCacheName + cacheVersion;
 let allCaches       = [currentCache, currentImages];
 
-// when SW is installing
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(currentCache)
-    .then(cache => {
-      return cache.addAll([
-        '/',
-        'js/prod/index.min.js',
-        'js/prod/restaurant.min.js',
-        'index.html',
-        'restaurant.html',
-        'img/map-image.png'
-      ]);
-    })
+    .then(cache => cache.addAll([
+      '/',
+      'index.html',
+      'restaurant.html'
+    ]))
+    // .then(self.skipWaiting())
   );
 });
 
-
-// when SW is activating
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys()
-    .then(cacheNames => {
+    caches.keys().then(cacheNames => {
+      return cacheNames.filter(cacheName => !allCaches.includes(cacheName));
+    }).then(cacheNames => {
       return Promise.all(
-        cacheNames.filter(cacheName => {
-          console.log('Cache Name:', cacheName);
-          return cacheName.startsWith(staticCacheName) && !allCaches.includes(cacheName);
-        }).map(cacheName => {
-          return caches.delete(cacheName);
+        cacheNames.map(cache => {
+          return caches.delete(cacheToDelete);
         })
       );
-    })
+    }).then(() => self.clients.claim())
   );
 });
 
-self.addEventListener('error', e => {
-  console.log(e);
-});
-
-
-// when the client requests something
 self.addEventListener('fetch', event => {
   var requestUrl = new URL(event.request.url);
-  if (requestUrl.origin === location.origin) {
-    if (requestUrl.pathname.endsWith('jpg')) {
-      event.respondWith(servePhoto(event.request));
-      return;
-    }
+  if (requestUrl.origin === self.location.origin) {
+    event.respondWith(
+      caches.match(requestUrl).then(response => {
+        if (response) return response;
+        return servePhoto(event.request)
+      })
+    );
   }
-  event.respondWith(
-		caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
 });
 
-
-// fetch and cache images
 function servePhoto(request) {
   let imageURL = request.url
 
   return caches.open(currentImages)
   .then(cache => {
-    return cache.match(imageURL)
-    .then(response => {
-      if (response) return response;
-      return fetch(request)
-      .then(networkResponse => {
-        cache.put(imageURL, networkResponse.clone());
+    return fetch(imageURL)
+    .then(networkResponse => {
+      return cache.put(imageURL, networkResponse.clone())
+      .then(() => {
         return networkResponse;
       });
     });
